85ac3c1c8aa0077ff909f86bab6b19ef1ade8e05
SimpleAstronaut <escscience@163.com>
2022-05-07 15:13:05 +0800

    update

--- public/main.js
+++ public/main.js
@@ -1,10 +1,20 @@
 function send(){
     var user = document.getElementById("user").value;
     var msg = document.getElementById("text").value;
-    $.get("http://127.0.0.1/add", { user : user , msg :msg }, function( data ,status ){
-        var sta = 1;
-        if (sta ==1){
-            alert(status);
-        }
+    $.get("http://127.0.0.1:8081/add", { user : user , msg :msg }, function( data ,status ){
+        console.log(status);
     })
-}s
+}
+
+function re(){
+    var msg;
+    $.get("http://127.0.0.1:8081/get", function( data , status ) {
+        num = Object.keys(data).length;//获取返回留言长度
+        console.log(data[num]);
+        var msg = data[0].user + data[0].msg + "-";
+        for (var i=1; i < num; i++){
+            msg = msg + data[i].user + data[i].msg + "-";
+        }
+        console.log(msg);
+    })
+}
